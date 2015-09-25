fa078a916c1df657cc15f75ead90d355d5363600
Kirill Korobchinskiy <kirill.korobchinskiy@gmail.com>
2015-09-25 19:53:42 +0300

    Crete New Project
    C# test project

---
+++ VolskayaGlushakov.KhaiDotNet.JsonSerializer/VolskayaGlushakov.KhaiDotNet.JsonSerializer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolskayaGlushakov.KhaiDotNet.JsonSerializer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
